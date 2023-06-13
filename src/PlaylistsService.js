e683db3135d16a6c0f39bbb2d4f53d20837490e3
Yunita Sari <102893382+yunitasari193310027@users.noreply.github.com>
2023-06-13 10:12:52 +0700

    Update PlaylistsService.js
    delete command

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -30,37 +30,3 @@ class PlaylistsService {
 }
  
 module.exports = PlaylistsService;
-/*
-const { Pool } = require('pg');
- 
-class PlaylistsService {
-  constructor() {
-    this._pool = new Pool();
-  }
- 
-  async getPlaylistById(id) {
-    const query = {
-      text: `SELECT playlists.id, playlists.name FROM playlists
-      LEFT JOIN users ON users.id = playlists.owner
-      WHERE playlists.id = $1;`,
-      values: [id],
-    };
-    const result = await this._pool.query(query);
-    return result.rows[0];
-  }
-
-  async getPlaylistSong(playlistId) {
-    const query = {
-      text: `SELECT s.id, s.title, s.performer FROM songs s
-      LEFT JOIN playlist_songs ps ON ps.song_id = s.id
-      WHERE ps.playlist_id = $1
-      GROUP BY s.id`,
-      values: [playlistId],
-    };
-    const result = await this._pool.query(query);
-    return result.rows;   
-  }
-}
- 
-module.exports = PlaylistsService;
-*/
